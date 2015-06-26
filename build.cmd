7fdbfd02937169a8edef0843f34a8df62a07912f
Matthew Leibowitz <mattleibow@live.com>
2015-06-26 23:42:50 +0200

    Added the Picasso component files and various icon sizes

--- build.cmd
+++ build.cmd
@@ -5,7 +5,12 @@ echo Building the solution
 msbuild binding\Square.sln /p:Configuration=Release /t:Rebuild
 
 rem build the nuget
-echo Packaging the NuGet
+echo Packaging the NuGets
 nuget pack nuget\Square.OkIO.nuspec
 nuget pack nuget\Square.OkHttp.nuspec
 nuget pack nuget\Square.Picasso.nuspec
+
+rem build the components
+echo Packaging the Components
+xamarin-component package component\square.picasso
+move component\square.picasso\*.xam .\
