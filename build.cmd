288b6037d34b61f8ff28bc279413703985eb338c
Matthew Leibowitz <mattleibow@live.com>
2015-07-16 15:37:41 +0200

    Fixed the Windows build (fixes for cross-platform)

--- build.cmd
+++ build.cmd
@@ -1,5 +1,12 @@
 echo off
 
+echo Setting up
+rem clean up before packaging
+rmdir /Q /S build
+rmdir /Q /S nuget\build
+rem make sure all the folders are in place
+mkdir build
+
 rem download the files
 
 rem check out any files
@@ -10,21 +17,39 @@ rem build the solution
 echo Building the solution
 msbuild binding\Square.sln /p:Configuration=Release /t:Rebuild
 
+rem copy the output for NuGet (so that we can avoid Windows/Mac path issues)
+echo Copying the output files packaging
+mkdir nuget\build
+copy README.md nuget\build
+copy LICENSE nuget\build
+copy binding\Square.OkIO\bin\Release\Square.OkIO.dll nuget\build
+copy binding\Square.OkHttp\bin\Release\Square.OkHttp.dll nuget\build
+copy binding\Square.Picasso\bin\Release\Square.Picasso.dll nuget\build
+copy binding\Square.OkHttp.WS\bin\Release\Square.OkHttp.WS.dll nuget\build
+copy binding\Square.SocketRocket\bin\Release\Square.SocketRocket.dll nuget\build
+
 rem build the nuget
 echo Packaging the NuGets
-nuget pack nuget\Square.OkIO.nuspec
-nuget pack nuget\Square.OkHttp.nuspec
-nuget pack nuget\Square.Picasso.nuspec
-nuget pack nuget\Square.OkHttp.WS.nuspec
-nuget pack nuget\Square.SocketRocket.nuspec
+nuget pack nuget\Square.OkIO.nuspec -OutputDirectory build
+nuget pack nuget\Square.OkHttp.nuspec -OutputDirectory build
+nuget pack nuget\Square.Picasso.nuspec -OutputDirectory build
+nuget pack nuget\Square.OkHttp.WS.nuspec -OutputDirectory build
+nuget pack nuget\Square.SocketRocket.nuspec -OutputDirectory build
 
 rem build the components
 echo Packaging the Components
 xamarin-component package component\square.picasso
-move component\square.picasso\*.xam .\
 xamarin-component package component\square.okhttp
-move component\square.okhttp\*.xam .\
 xamarin-component package component\square.okhttp.ws
-move component\square.okhttp.ws\*.xam .\
 xamarin-component package component\square.socketrocket
-move component\square.socketrocket\*.xam .\
+
+rem move the files to the output location
+echo Moving files to the build directory
+move component\square.picasso\*.xam build
+move component\square.okhttp.ws\*.xam build
+move component\square.okhttp\*.xam build
+move component\square.socketrocket\*.xam build
+
+rem clean any temporary files/folders
+echo Cleaning up
+rmdir /Q /S nuget\build
