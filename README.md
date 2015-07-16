4e63d0eaedf68950e9a6b53a12c0543a9c93a6ff
Matthew Leibowitz <mattleibow@live.com>
2015-07-16 18:19:39 +0200

    updated the mac script

--- README.md
+++ README.md
@@ -5,23 +5,37 @@ Xamarin.Android and Xamarin.iOS bindings for the various https://github.com/squa
 Bound:
 
  - **Picasso** - https://github.com/square/picasso  
-   [binding][1] | [sample][2]  
+   A powerful image downloading and caching library for Android  
+   [binding][1] | [sample][2] | [NuGet][10] | [Xamarin Component][15]  
  - **OkHttp** - https://github.com/square/okhttp  
-   [binding][3] | [sample][7]  
+   An HTTP+SPDY client for Android and Java applications  
+   [binding][3] | [sample][7] | [NuGet][11] | [Xamarin Component][16]  
  - **OkHttp.WS** - https://github.com/square/okhttp/tree/parent-2.4.0/okhttp-ws  
-   [binding][5] | [sample][6]  
+   A RFC6455-compliant web socket implementation  
+   [binding][5] | [sample][6] | [NuGet][13] | [Xamarin Component][17]  
  - **OkIO** - https://github.com/square/okio  
-   [binding][4]  
+   A modern I/O API for Java  
+   [binding][4] | [NuGet][12]    
  - **SocketRocket** - https://github.com/square/SocketRocket  
-   [binding][8] | [sample][9]  
+   A conforming WebSocket (RFC 6455) client library  
+   [binding][8] | [sample][9] | [NuGet][14] | [Xamarin Component][18]  
 
 
-[1]: https://github.com/mattleibow/square-bindings/tree/master/binding/Square.Picasso
-[2]: https://github.com/mattleibow/square-bindings/tree/master/sample/PicassoSample
-[3]: https://github.com/mattleibow/square-bindings/tree/master/binding/Square.OkHttp
-[4]: https://github.com/mattleibow/square-bindings/tree/master/binding/Square.OkIO
-[5]: https://github.com/mattleibow/square-bindings/tree/master/binding/Square.OkHttp.WS
-[6]: https://github.com/mattleibow/square-bindings/tree/master/sample/OkHttpWSSample
-[7]: https://github.com/mattleibow/square-bindings/tree/master/sample/OkHttpSample
-[8]: https://github.com/mattleibow/square-bindings/tree/master/binding/Square.SocketRocket
-[9]: https://github.com/mattleibow/square-bindings/tree/master/sample/SocketRocketSample
+[1]:  https://github.com/mattleibow/square-bindings/tree/master/binding/Square.Picasso
+[2]:  https://github.com/mattleibow/square-bindings/tree/master/sample/PicassoSample
+[3]:  https://github.com/mattleibow/square-bindings/tree/master/binding/Square.OkHttp
+[4]:  https://github.com/mattleibow/square-bindings/tree/master/binding/Square.OkIO
+[5]:  https://github.com/mattleibow/square-bindings/tree/master/binding/Square.OkHttp.WS
+[6]:  https://github.com/mattleibow/square-bindings/tree/master/sample/OkHttpWSSample
+[7]:  https://github.com/mattleibow/square-bindings/tree/master/sample/OkHttpSample
+[8]:  https://github.com/mattleibow/square-bindings/tree/master/binding/Square.SocketRocket
+[9]:  https://github.com/mattleibow/square-bindings/tree/master/sample/SocketRocketSample
+[10]: https://www.nuget.org/packages/Square.Picasso
+[11]: https://www.nuget.org/packages/Square.OkHttp
+[12]: https://www.nuget.org/packages/Square.OkIO
+[13]: https://www.nuget.org/packages/Square.OkHttp.WS
+[14]: https://www.nuget.org/packages/Square.SocketRocket
+[15]: http://components.xamarin.com/view/square.picasso
+[16]: http://components.xamarin.com/view/square.okhttp
+[17]: http://components.xamarin.com/view/square.okhttp.ws
+[18]: http://components.xamarin.com/view/square.socketrocket
--- build.sh
+++ build.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
+echo Setting up
+# clean up before packaging
+rm -rf build
+rm -rf nuget/build
+# make sure all the folders are in place
+mkdir build
+
 # download the files
 echo Downloading binaries
-curl -L http://search.maven.org/remotecontent?filepath=com/squareup/okio/okio/1.5.0/okio-1.5.0.jar > binding/Square.OkIO/Jars/okio-1.5.0.jar
-curl -L http://search.maven.org/remotecontent?filepath=com/squareup/okhttp/okhttp/2.4.0/okhttp-2.4.0.jar > binding/Square.OkHttp/Jars/okhttp-2.4.0.jar
-curl -L http://search.maven.org/remotecontent?filepath=com/squareup/okhttp/okhttp-ws/2.4.0/okhttp-ws-2.4.0.jar > binding/Square.OkHttp.WS/Jars/okhttp-ws-2.4.0.jar
-curl -L http://search.maven.org/remotecontent?filepath=com/squareup/picasso/picasso/2.5.2/picasso-2.5.2.jar > binding/Square.Picasso/Jars/picasso-2.5.2.jar
+if [ ! -f binding/Square.OkIO/Jars/okio-1.5.0.jar ]; then
+    curl -L http://search.maven.org/remotecontent?filepath=com/squareup/okio/okio/1.5.0/okio-1.5.0.jar > binding/Square.OkIO/Jars/okio-1.5.0.jar
+fi
+if [ ! -f binding/Square.OkHttp/Jars/okhttp-2.4.0.jar ]; then
+    curl -L http://search.maven.org/remotecontent?filepath=com/squareup/okhttp/okhttp/2.4.0/okhttp-2.4.0.jar > binding/Square.OkHttp/Jars/okhttp-2.4.0.jar
+fi
+if [ ! -f binding/Square.OkHttp.WS/Jars/okhttp-ws-2.4.0.jar ]; then
+    curl -L http://search.maven.org/remotecontent?filepath=com/squareup/okhttp/okhttp-ws/2.4.0/okhttp-ws-2.4.0.jar > binding/Square.OkHttp.WS/Jars/okhttp-ws-2.4.0.jar
+fi
+if [ ! -f binding/Square.Picasso/Jars/picasso-2.5.2.jar ]; then
+    curl -L http://search.maven.org/remotecontent?filepath=com/squareup/picasso/picasso/2.5.2/picasso-2.5.2.jar > binding/Square.Picasso/Jars/picasso-2.5.2.jar
+fi
 
 # check out any files
 echo Downloading source code
-git clone https://github.com/square/SocketRocket.git binding/Square.SocketRocket/Archives/SocketRocket
-(cd ./binding/Square.SocketRocket/Archives/SocketRocket &&
-    git --git-dir=.git checkout 28719c9719cb2ca857f3130d343e3c5326eb2cc3)
+if [ ! -d binding/Square.SocketRocket/Archives/SocketRocket/.git ]; then
+    git clone https://github.com/square/SocketRocket.git binding/Square.SocketRocket/Archives/SocketRocket
+    (cd ./binding/Square.SocketRocket/Archives/SocketRocket &&
+        git --git-dir=.git checkout 28719c9719cb2ca857f3130d343e3c5326eb2cc3)
+fi
 
 # build any native libraries
 echo Building native libraries
@@ -32,26 +49,41 @@ echo Building native libraries
 
 # build the solution
 echo Building the solution
-(cd ./binding &&
-    xbuild Square.sln /p:Configuration=Release /t:Rebuild
+xbuild binding/Square.sln /p:Configuration=Release /t:Rebuild
+
+# copy the output for NuGet (so that we can avoid Windows/Mac path issues)
+echo Copying the output files packaging
+mkdir nuget/build
+cp README.md nuget/build
+cp LICENSE nuget/build
+cp binding/Square.OkIO/bin/Release/Square.OkIO.dll nuget/build
+cp binding/Square.OkHttp/bin/Release/Square.OkHttp.dll nuget/build
+cp binding/Square.Picasso/bin/Release/Square.Picasso.dll nuget/build
+cp binding/Square.OkHttp.WS/bin/Release/Square.OkHttp.WS.dll nuget/build
+cp binding/Square.SocketRocket/bin/Release/Square.SocketRocket.dll nuget/build
 
 # build the nuget
 echo Packaging the NuGets
-(cd ./nuget &&
-    nuget pack Square.OkIO.nuspec -BasePath ./ &&
-    nuget pack Square.OkHttp.nuspec -BasePath ./ &&
-    nuget pack Square.Picasso.nuspec -BasePath ./ &&
-    nuget pack Square.OkHttp.WS.nuspec -BasePath ./ &&
-    nuget pack Square.SocketRocket.nuspec -BasePath ./)
+nuget pack nuget/Square.OkIO.nuspec -BasePath nuget -OutputDirectory build
+nuget pack nuget/Square.OkHttp.nuspec -BasePath nuget -OutputDirectory build
+nuget pack nuget/Square.Picasso.nuspec -BasePath nuget -OutputDirectory build
+nuget pack nuget/Square.OkHttp.WS.nuspec -BasePath nuget -OutputDirectory build
+nuget pack nuget/Square.SocketRocket.nuspec -BasePath nuget -OutputDirectory build
 
 # build the components
 echo Packaging the Components
-(cd ./component &&
-    xamarin-component package square.picasso &&
-    mv square.picasso/*.xam ../ &&
-    xamarin-component package square.okhttp &&
-    mv square.okhttp/*.xam ../ &&
-    xamarin-component package square.okhttp.ws &&
-    mv square.okhttp.ws/*.xam ../ &&
-    xamarin-component package square.socketrocket &&
-    mv square.socketrocket/*.xam ../)
+xamarin-component package component/square.picasso
+xamarin-component package component/square.okhttp
+xamarin-component package component/square.okhttp.ws
+xamarin-component package component/square.socketrocket
+
+# move the files to the output location
+echo Moving files to the build directory
+mv component/square.picasso/*.xam build
+mv component/square.okhttp.ws/*.xam build
+mv component/square.okhttp/*.xam build
+mv component/square.socketrocket/*.xam build
+
+# clean any temporary files/folders
+echo Cleaning up
+rm -rf nuget/build
