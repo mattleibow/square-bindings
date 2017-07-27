b92af81ced09abc49caf691e57c4ea141cb24920
Matthew Leibowitz <mattleibow@live.com>
2017-07-27 08:04:42 +0200

    [SocketRocket] update sample

--- sample/SocketRocketSample/ViewController.cs
+++ sample/SocketRocketSample/ViewController.cs
@@ -64,6 +64,7 @@ namespace SocketRocketSample
 				webSocket.WebSocketClosed += (_, e) => {
 					logTextField.Text += string.Format ("Disconnected: '{1}' ({2}).{0}", Environment.NewLine, e.Reason, e.Code);
 					UpdateUI (ReadyState.Closed);
+					webSocket = null;
 				};
 				webSocket.WebSocketFailed += (_, e) => {
 					logTextField.Text += string.Format ("Failed to connect: {1}.{0}", Environment.NewLine, e.Error);
