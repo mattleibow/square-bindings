67e9846f54199fdf9dc4dce45fc14f9e3481576d
Matthew Leibowitz <mattleibow@live.com>
2017-07-27 06:09:52 +0200

    [OkHttp3] Update OkHttp3 to v3.8.1

--- binding/Square.OkHttp3/Properties/AssemblyInfo.cs
+++ binding/Square.OkHttp3/Properties/AssemblyInfo.cs
@@ -27,4 +27,4 @@ using Android.App;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.8.0.0")]
-[assembly: AssemblyFileVersion("3.8.0.0")]
+[assembly: AssemblyFileVersion("3.8.1.0")]
