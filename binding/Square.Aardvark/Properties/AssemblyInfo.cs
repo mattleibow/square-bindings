7e44e99b8f19402c759d227b00b1ea4b3ce17439
Matthew Leibowitz <mattleibow@live.com>
2017-08-01 23:44:01 +0200

    [Aardvark] Updated Aardvark to v3.1.0

--- binding/Square.Aardvark/Properties/AssemblyInfo.cs
+++ binding/Square.Aardvark/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using Foundation;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion ("3.0.0.0")]
-[assembly: AssemblyFileVersion ("3.0.0.0")]
+[assembly: AssemblyFileVersion ("3.1.0.0")]
