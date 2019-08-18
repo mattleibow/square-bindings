86ed4e485a31a9308cee8610c65aed03c3123dc6
Matthew Leibowitz <mattleibow@live.com>
2019-08-18 05:32:56 +0200

    Revert " Update OkIO to v2.0 (#43)"
    This reverts commit 42384e81d67deb124ec8fb6d11e6b42b79c50d9e.

--- binding/Square.OkIO/OkBuffer.cs
+++ binding/Square.OkIO/OkBuffer.cs
@@ -1,45 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
 using Java.Nio.Charset;
 
 namespace Square.OkIO
 {
     partial class OkBuffer
     {
-        IBufferedSink IBufferedSink.Emit() => Emit();
+        IBufferedSink IBufferedSink.EmitCompleteSegments()
+        {
+            return EmitCompleteSegments();
+        }
 
-        IBufferedSink IBufferedSink.EmitCompleteSegments() => EmitCompleteSegments();
+        IBufferedSink IBufferedSink.Write(byte[] p0)
+        {
+            return Write(p0);
+        }
 
-        IBufferedSink IBufferedSink.Write(byte[] source) => Write(source);
+        IBufferedSink IBufferedSink.Write(byte[] p0, int p1, int p2)
+        {
+            return Write(p0, p1, p2);
+        }
 
-        IBufferedSink IBufferedSink.Write(byte[] source, int offset, int byteCount) => Write(source, offset, byteCount);
+        IBufferedSink IBufferedSink.Write(ByteString p0)
+        {
+            return Write(p0);
+        }
 
-        IBufferedSink IBufferedSink.Write(ByteString byteString) => Write(byteString);
+        IBufferedSink IBufferedSink.WriteByte(int p0)
+        {
+            return WriteByte(p0);
+        }
 
-        IBufferedSink IBufferedSink.WriteByte(int b) => WriteByte(b);
+        IBufferedSink IBufferedSink.WriteDecimalLong(long p0)
+        {
+            return WriteDecimalLong(p0);
+        }
 
-        IBufferedSink IBufferedSink.WriteDecimalLong(long v) => WriteDecimalLong(v);
+        IBufferedSink IBufferedSink.WriteHexadecimalUnsignedLong(long p0)
+        {
+            return WriteHexadecimalUnsignedLong(p0);
+        }
 
-        IBufferedSink IBufferedSink.WriteHexadecimalUnsignedLong(long v) => WriteHexadecimalUnsignedLong(v);
+        IBufferedSink IBufferedSink.WriteInt(int p0)
+        {
+            return WriteInt(p0);
+        }
 
-        IBufferedSink IBufferedSink.WriteInt(int i) => WriteInt(i);
+        IBufferedSink IBufferedSink.WriteIntLe(int p0)
+        {
+            return WriteIntLe(p0);
+        }
 
-        IBufferedSink IBufferedSink.WriteIntLe(int i) => WriteIntLe(i);
+        IBufferedSink IBufferedSink.WriteLong(long p0)
+        {
+            return WriteLong(p0);
+        }
 
-        IBufferedSink IBufferedSink.WriteLong(long v) => WriteLong(v);
+        IBufferedSink IBufferedSink.WriteLongLe(long p0)
+        {
+            return WriteLongLe(p0);
+        }
 
-        IBufferedSink IBufferedSink.WriteLongLe(long v) => WriteLongLe(v);
+        IBufferedSink IBufferedSink.WriteShort(int p0)
+        {
+            return WriteShort(p0);
+        }
 
-        IBufferedSink IBufferedSink.WriteShort(int s) => WriteShort(s);
+        IBufferedSink IBufferedSink.WriteShortLe(int p0)
+        {
+            return WriteShortLe(p0);
+        }
 
-        IBufferedSink IBufferedSink.WriteShortLe(int s) => WriteShortLe(s);
+        IBufferedSink IBufferedSink.WriteString(string p0, int p1, int p2, Charset p3)
+        {
+            return WriteString(p0,p1, p2, p3);
+        }
 
-        IBufferedSink IBufferedSink.WriteString(string str, int beginIndex, int endIndex, Charset charset) => WriteString(str, beginIndex, endIndex, charset);
+        IBufferedSink IBufferedSink.WriteString(string p0, Charset p1)
+        {
+            return WriteString(p0,p1);
+        }
 
-        IBufferedSink IBufferedSink.WriteString(string str, Charset charset) => WriteString(str, charset);
+        IBufferedSink IBufferedSink.WriteUtf8(string p0)
+        {
+            return WriteUtf8(p0);
+        }
 
-        IBufferedSink IBufferedSink.WriteUtf8(string str) => WriteUtf8(str);
+        IBufferedSink IBufferedSink.WriteUtf8(string p0, int p1, int p2)
+        {
+            return WriteUtf8(p0, p1, p2);
+        }
 
-        IBufferedSink IBufferedSink.WriteUtf8(string str, int beginIndex, int endIndex) => WriteUtf8(str, beginIndex, endIndex);
-
-        IBufferedSink IBufferedSink.WriteUtf8CodePoint(int codePoint) => WriteUtf8CodePoint(codePoint);
+        IBufferedSink IBufferedSink.WriteUtf8CodePoint(int p0)
+        {
+            return WriteUtf8CodePoint(p0);
+        }
     }
 }
